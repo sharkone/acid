4f77290596407d37c874141223d05b6b85ad4fc1
Thomas Texier <sharkone@en-mousse.org>
2013-07-08 23:18:00 -0400

    Fixed release build

--- src/network/precomp/precomp.h
+++ src/network/precomp/precomp.h
@@ -1,7 +1,11 @@
 #pragma once
 
+#include <cstdio>
+#include <iostream>
 #include <string>
 #include <vector>
+
+#define _WIN32_WINNT    0x0501
 
 #include <boost/asio.hpp>
 #include <boost/lexical_cast.hpp>
